9a95f0f2e578c918a809f76f83667bc303916908
LionelErasUNTREF <erasun66914@estudiantes.untref.edu.ar>
2022-06-09 17:27:10 -0300

    Algunas correcciones/mejoras
    Emprolijo algo de la lógica/mensaje de excepciones/formato

--- LectorLiga.java
+++ LectorLiga.java
@@ -8,50 +8,42 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class LectorLiga {
-// la lista de competidores per viene del LectorCompetidor
-	public static List<Liga> main(String fileName, List<Competidor> per) throws MismoBandoException {
-		ArrayList<Liga> aux = new ArrayList<Liga>();
-try {
-			
-			FileReader archivo = new FileReader(fileName);
-			BufferedReader lector = new BufferedReader(archivo);
-			String[] datos;
-			String line = lector.readLine();
+    // la lista de competidores per viene del LectorCompetidor
+    public static List<Liga> main(String fileName, List<Competidor> per) throws MismoBandoException {
+        ArrayList<Liga> aux = new ArrayList<Liga>();
+        try {
+            FileReader archivo = new FileReader(fileName);
+            BufferedReader lector = new BufferedReader(archivo);
+            String[] datos;
+            String line = lector.readLine();
 
-			while (line != null) {
+            while (line != null) {
+                datos = line.split(",");
+                boolean esHeroe = false; // arranca false para no tener que setear de nuevo, solo cambia si viene un superheroe
+                if (datos[0].equals("Heroe"))
+                    esHeroe = true;
+                Liga liga = new Liga(datos[1], esHeroe);
+                for (int i = 2; i < datos.length; i++) {
+                    for (Competidor comp : per) {
+                        if (comp.getNombre() == datos[i])
+                            liga.agregarCompetidor(comp);
+                    }
+                }
+                aux.add(liga);
+                line = lector.readLine();
+            }
+            lector.close();
 
-				datos = line.split(",");
-				boolean esHeroe = true;
-				if (datos[0].equals("Heroe"))
-					esHeroe = true;
-				if (datos[0].equals("Villano"))
-					esHeroe = false;
-				Liga liga = new Liga(datos[1],esHeroe);
-				for(int i = 2; i<datos.length;i++) {
-					for (Competidor comp : per) {
-						if(comp.getNombre()==datos[i])
-							liga.agregarCompetidor(comp);
-					}
-					
-				}
-				aux.add(liga);
-					
-				line = lector.readLine();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encontro el archivo " + fileName + ". " + e);
+        } catch (NumberFormatException e1) {
+            System.out.println("Se han introducido caracteres no numericos. " + e1);
+        } catch (MismoBandoException e2) {
 
-			}
-			lector.close();
-
-		}catch (FileNotFoundException e) {
-			System.out.println("pasalo bien flaco "+ e);
-		}catch(NumberFormatException e1){
-            System.out.println("Se han introducido caracteres no numéricos " + e1);
-		}catch (MismoBandoException e2) {
-			
         } catch (IOException e3) {
-				e3.printStackTrace();
-		}
-		
-		return aux;
-	}
+            e3.printStackTrace();
+        }
+        return aux;
+    }
 
 }
